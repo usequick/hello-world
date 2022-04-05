f07878f17b99ead72a793a602f4559913958bb93
usequick <103016127+usequick@users.noreply.github.com>
2022-04-05 15:18:42 +0800

    Add files via upload

---
+++ c++基础程序/02 注释.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "HELLO WORLD!" << endl;//单行注释
+//	/*
+//		多行注释
+//	*/
+//
+//	return 0;
+//}
---
+++ c++基础程序/04 常量.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define G_WEIQI_9 3800
+
+int main()
+{
+	const int DW_weiqi = 9;
+
+	cout << "你的棋力为 : " << G_WEIQI_9 << endl;
+	cout << "你的段位是 : " << DW_weiqi << endl;
+
+	return 0;
+}
